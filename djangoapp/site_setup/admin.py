08742aa5517d1b4185db99871805c919b00f15fa
Wanderson Rossetti <pastarossetti@gmail.com>
2023-07-22 14:55:26 -0300

    create MenuLinkInline

--- djangoapp/site_setup/admin.py
+++ djangoapp/site_setup/admin.py
@@ -9,9 +9,15 @@ class MenuLinkAdmin(admin.ModelAdmin):
     search_fields = 'id', 'text', 'url_or_path',
 
 
+class MenulinkInline(admin.TabularInline):
+    model = MenuLink
+    extra = 1
+
+
 @admin.register(SiteSetup)
 class SiteSetupAdmin(admin.ModelAdmin):
     list_display = 'title', 'description',
+    inlines = MenulinkInline,
 
     def has_add_permission(self, request):
         return not SiteSetup.objects.exists()
