7731814e27af4f679faaa7d75f8aa29f018b70ee
Wanderson Rossetti <pastarossetti@gmail.com>
2023-07-23 22:11:29 -0300

    creating urls page & post

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('page/', views.page, name='page'),
+    path('post/', views.post, name='post'),
 ]
