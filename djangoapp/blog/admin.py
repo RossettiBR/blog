9cb11586d1196550638c193d90990f0f658c67d2
Wanderson Rossetti <pastarossetti@gmail.com>
2023-07-25 11:50:00 -0300

    update

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Category, Tag, Page
+from blog.models import Category, Tag, Page, Posts
 
 
 @admin.register(Category)
@@ -36,3 +36,19 @@ class PageAdmin(admin.ModelAdmin):
     prepopulated_fields = {
         "slug": ('title',),
     }
+
+
+@admin.register(Posts)
+class PostAdmin(admin.ModelAdmin):
+    list_display = 'id', 'title', 'is_published',  'created_by',
+    list_display_links = 'title',
+    search_fields = 'id', 'slug', 'title', 'excerpt', 'content',
+    list_per_page = 50
+    list_filter = 'category', 'is_published',
+    list_editable = 'is_published',
+    ordering = '-id',
+    readonly_fields = 'created_at', 'updated_at', 'created_by', 'updated_by',
+    prepopulated_fields = {
+        "slug": ('title',),
+    }
+    autocomplete_fields = 'tags', 'category',
