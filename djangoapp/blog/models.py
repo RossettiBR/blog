a7a8a215042f4c1f70dbb574d3ea7a2c2350a1ff
Wanderson Rossetti <pastarossetti@gmail.com>
2023-07-24 23:44:51 -0300

    update

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from utils.rands import slugify_new
 
 
-class Tag(models.Model):
+class Category(models.Model):
     class Meta:
-        verbose_name = 'Tag'
-        verbose_name_plural = 'Tags'
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
 
     name = models.CharField(max_length=255)
     slug = models.SlugField(
@@ -15,6 +15,18 @@ class Tag(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.slug:
+            self.slug = slugify_new(self.name, 4)
+        return super().save(*args, **kwargs)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(
+        default=None, null=True, blank=True,
+        max_length=255, unique=True,
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
             self.slug = slugify_new(self.name)
         return super().save(*args, **kwargs)
-    
