d0bb741d05763a0dcbb2fea48a1759de86ff49dd
Wanderson Rossetti <pastarossetti@gmail.com>
2023-07-27 20:05:51 -0300

    update

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('', views.PostListView.as_view(), name='index'),
     path('page/<slug:slug>/', views.page, name='page'),
     path('post/<slug:slug>/', views.post, name='post'),
-    path('created_by/<int:author_pk>', views.CreatedByListView.as_view(), name='created_by'),
-    path('category/<slug:slug>', views.category, name='category'),
+    path('created_by/<int:author_pk>/', views.CreatedByListView.as_view(), name='created_by'),
+    path('category/<slug:slug>/', views.CategoryListView.as_view(), name='category'),
     path('tag/<slug:slug>', views.tag, name='tag'),
     path('search/', views.search, name='search'),
 ]
